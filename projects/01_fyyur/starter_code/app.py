a0f68be5766ceb8f6c283b79dde258ae8f4cf401
Bomarzoog <bomarzoogo@gmail.com>
2022-01-05 00:24:35 -0600

    commit

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -119,7 +119,7 @@ def venues():
     data =[]
     
     for i in venues:
-      ven_city = Venue.query.filter_by(city=i.city).order_by('city').distinct()
+      ven_city = Venue.query(distinct(Venue.city)).filter_by(city=i.city).order_by('city').all()
       data += [{
         "city":i.city,"state":i.state,"venues": ven_city
         }]
