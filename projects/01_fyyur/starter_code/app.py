68a02a4be0cadf766ac803e7269e2ddbbe5696c4
Bomarzoog <bomarzoogo@gmail.com>
2022-01-05 14:55:46 -0600

    commit

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -96,8 +96,8 @@ class Artist(db.Model):
 
 class Genres(db.Model):
   __tablename__ = 'Genres'
-      id = db.Column(db.Integer, primary_key=True)
-      name = db.Column(db.String)
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String)
 
 
 
