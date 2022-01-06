6d0549048eb74c144f24046de69f16aa2343959e
Bomarzoog <bomarzoogo@gmail.com>
2022-01-06 00:10:24 -0600

    commit

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -91,25 +91,15 @@ class Show(db.Model):
 # Filters.
 #----------------------------------------------------------------------------#
 
-#def format_datetime(value, format='medium'):
-#  date = dateutil.parser.parse(value)
-#  if format == 'full':
-#      format="EEEE MMMM, d, y 'at' h:mma"
-#  elif format == 'medium':
-#      format="EE MM, dd, y h:mma"
-#  return babel.dates.format_datetime(date, format, locale='en')
-#
-#
-#
-#app.jinja_env.filters['datetime'] = format_datetime
-
-
 def format_datetime(value, format='medium'):
-    # instead of just date = dateutil.parser.parse(value)
-    if isinstance(value, str):
-        date = dateutil.parser.parse(value)
-    else:
-        date = value
+  date = dateutil.parser.parse(value)
+  if format == 'full':
+      format="EEEE MMMM, d, y 'at' h:mma"
+  elif format == 'medium':
+      format="EE MM, dd, y h:mma"
+  return babel.dates.format_datetime(date, format, locale='en')
+
+app.jinja_env.filters['datetime'] = format_datetime
 
 #----------------------------------------------------------------------------#
 # Controllers.
