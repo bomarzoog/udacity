7bfe9ee3b66aa005e7dbf15b34979fc1bf565063
Bomarzoog <bomarzoogo@gmail.com>
2022-01-06 09:45:04 -0600

    commit

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -173,8 +173,8 @@ def search_venues():
 @app.route('/venues/<int:venue_id>')
 def show_venue(venue_id):
 
-  past_show_query = Show.query.filter_by(venue_id=venue_id).filter(Show.start_time < datetime.now()).all()
-  upcoming_show_query = Show.query.filter_by(venue_id=venue_id).filter(Show.start_time > datetime.now()).all()
+  past_show_query = Show.query.distinct(Show.start_time).filter_by(venue_id=venue_id).filter(Show.start_time < datetime.now()).all()
+  upcoming_show_query = Show.query.distinct(Show.start_time).filter_by(venue_id=venue_id).filter(Show.start_time > datetime.now()).all()
   past_shows = []
   past_count = 0
   upcoming_count = 0
