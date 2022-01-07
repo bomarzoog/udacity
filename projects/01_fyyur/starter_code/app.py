1b9c4c05bc80d28c39ae7d07c2abc69f6d66746b
Bomarzoog <bomarzoogo@gmail.com>
2022-01-07 00:44:49 -0600

    commit

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -346,7 +346,7 @@ def show_venue(venue_id):
 
 @app.route('/venues/create', methods=['GET'])
 def create_venue_form():
-  form = VenueForm()
+  form = VenueForm(csrf_enabled=False)
   return render_template('forms/new_venue.html', form=form)
 
 @app.route('/venues/create', methods=['POST'])
