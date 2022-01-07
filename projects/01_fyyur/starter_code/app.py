999ad75d1b468f0a10bed49c0d7c9668701bc9fc
Bomarzoog <bomarzoogo@gmail.com>
2022-01-07 17:05:46 -0600

    Delete Button

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -414,7 +414,7 @@ def delete_venue(venue_id):
     flash('An error occurred. Venue ' + venue_id + ' could not be deleted.')
   finally:
     db.session.close()
-    return render_template('pages/home.html')
+    return none
 
   
 
