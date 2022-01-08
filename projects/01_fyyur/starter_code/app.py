230563816ada7879c4cd6f4f1b5653e68beed4e5
Bomarzoog <bomarzoogo@gmail.com>
2022-01-08 12:49:55 -0600

    Delete Button

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -902,11 +902,39 @@ def create_shows():
 
 @app.route('/shows/create', methods=['POST'])
 def create_show_submission():
+
+try:
+    form = ShowForm(request.form,csrf_enabled=False)
+  
+    if form.validate_on_submit():
+      show = Show(
+        artist_id=form.artist_id.data,
+        venue_id=form.venue_id.data,
+        start_time=form.start_time.data
+        )
+    else: 
+      flash(form.errors)
+
+    db.session.add(show)
+    db.session.commit()
+    flash('Show was successfully listed!')
+  except:
+    db.session.rollback()
+    flash('An error occurred. Artist ' + form.name.data + ' could not be listed.')
+    print(sys.exc_info())
+
+
+  finally:
+    db.session.close()
+    return render_template('pages/home.html')
+
+
+
+  
   # called to create new shows in the db, upon submitting new show listing form
   # TODO: insert form data as a new Show record in the db, instead
 
   # on successful db insert, flash success
-  flash('Show was successfully listed!')
   # TODO: on unsuccessful db insert, flash an error instead.
   # e.g., flash('An error occurred. Show could not be listed.')
   # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
