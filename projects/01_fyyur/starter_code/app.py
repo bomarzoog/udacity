7d959ff19af962c54faf4dca34c8739d1a655d9f
Bomarzoog <bomarzoogo@gmail.com>
2022-01-08 12:20:03 -0600

    Delete Button

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -737,9 +737,46 @@ def edit_venue(venue_id):
 
 @app.route('/venues/<int:venue_id>/edit', methods=['POST'])
 def edit_venue_submission(venue_id):
+
+  try:
+    form = VenueForm(request.form,csrf_enabled=False)
+    venue =Venue.query.get(venue_id)
+  
+    if form.validate_on_submit():
+      
+      venue.name=form.name.data
+      venue.city=form.city.data
+      venue.state=form.state.data
+      venue.phone=form.phone.data
+      venue.image_link=form.image_link.data
+      venue.genres=form.genres.data
+      venue.facebook_link=form.facebook_link.data
+      venue.website=form.website_link.data
+      venue.seeking_talent=form.seeking_talent.data
+      venue.seeking_description=form.seeking_description.data
+      venue.address=form.address.data
+
+    
+    else: 
+      flash(form.errors)
+
+    db.session.add(venue)
+    db.session.commit()
+    flash('venue ' + request.form['name'] + ' was successfully listed!')
+  except:
+    db.session.rollback()
+    flash('An error occurred. venue ' + form.name.data + ' could not be listed.')
+    print(sys.exc_info())
+
+  finally:
+    db.session.close()
+    return redirect(url_for('show_venue', venue_id=venue_id))
+
+
+
+
   # TODO: take values from the form submitted, and update existing
   # venue record with ID <venue_id> using the new attributes
-  return redirect(url_for('show_venue', venue_id=venue_id))
 
 #  Create Artist
 #  ----------------------------------------------------------------
