ed2e01b8e9130fd7e897c80d82bdcd4b2410c6e0
Bomarzoog <bomarzoogo@gmail.com>
2022-01-13 07:59:58 -0600

    Delete Button

--- projects/01_fyyur/starter_code/models.py
+++ projects/01_fyyur/starter_code/models.py
@@ -24,7 +24,7 @@ class Venue(db.Model):
     website = db.Column(db.String(120))
     seeking_talent = db.Column(db.Boolean,default=False)
     seeking_description = db.Column(db.String(500))
-    show = db.relationship('Show',backref='venue', lazy='joined',cascade="all, delete")
+    shows = db.relationship('Show',backref='venue', lazy='joined',cascade="all, delete")
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
