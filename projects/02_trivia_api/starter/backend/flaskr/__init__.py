d14bbe1e56fc4231d7fd744cc174862d3fe78550
Bomarzoog <bomarzoogo@gmail.com>
2022-02-01 11:22:43 -0600

    J

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -55,7 +55,7 @@ def create_app(test_config=None):
     def retrive_questions():
       selection = Question.query.order_by(Question.id).all()
       current_questions = paginate_questions(request, selection)
-      categories_selection =  Category.query.order_by(Category.id).all()
+      categories_selection =  Category.query.order_by(Category.id).distinct.all()
       categories = {category.id:category.type for category in categories_selection}
       current_categories = [categories[id] for id in [question["category"] for question in current_questions ]]
       print (current_categories)
