ead597df8b12741f20845406b80ad6c85534ff25
Bomarzoog <bomarzoogo@gmail.com>
2022-01-31 22:13:37 -0600

    J

--- projects/02_trivia_api/starter/backend/models.py
+++ projects/02_trivia_api/starter/backend/models.py
@@ -3,8 +3,11 @@ from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
 
-database_name = "trivia"
-database_path = "postgres://{}/{}".format('localhost:5432', database_name)
+database_name = 'trivia'
+database_password = 'abc'
+database_user = 'postgres'
+#database_path = "postgres://{}/{}".format('localhost:5432', database_name)
+database_path = 'postgresql+psycopg2://{}:{}@{}/{}'.format(database_user, database_password, 'localhost:5432', database_name)
 
 db = SQLAlchemy()
 
