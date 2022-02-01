c339b0a0725654130c68c66819258c5615738638
Bomarzoog <bomarzoogo@gmail.com>
2022-02-01 09:52:54 -0600

    J

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -8,6 +8,16 @@ from models import setup_db, Question, Category
 
 QUESTIONS_PER_PAGE = 10
 
+def paginate_questions(request, selection):
+  page = request.args.get("page", 1, type=int)
+  start = (page - 1) * QUESTIONS_PER_PAGE
+  end = start + QUESTIONS_PER_PAGE
+
+  questions = [question.format() for question in selection]
+  current_questions = questions[start:end]
+
+  return current_questions
+
 def create_app(test_config=None):
   # create and configure the app
     app = Flask(__name__)
@@ -28,21 +38,34 @@ def create_app(test_config=None):
     def retrive_categories():
         categories = {}
         selection = Category.query.order_by(Category.id).all()
+        print(selection)
         for category in selection:
           categories[category.id] = category.type
         
         if len(categories) == 0:
           abort(404)
-        
+        print(categories)
         return jsonify(
           {
             "categories" : categories
           }
         )
+    
+    @app.route('/questions')
+    def retrive_questions():
+      selection = Question.query.order_by(Question.id).all()
+      current_questions = paginate_questions(request, selection)
+      #questions = [question.question for ]
 
+      if len(current_questions) == 0:
+        abort(404)
 
-
-
+      return jsonify(
+        {
+          "total_questions": len(current_questions),
+        
+        }
+      )
 
 
     '''
@@ -57,6 +80,7 @@ def create_app(test_config=None):
     ten questions per page and pagination at the bottom of the screen for three pages.
     Clicking on the page numbers should update the questions. 
     '''
+
 
     '''
     @TODO: 
