a1806950a3cccc59ce5341988275a5708fb34f2b
Bomarzoog <bomarzoogo@gmail.com>
2022-02-01 11:09:18 -0600

    J

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -57,7 +57,7 @@ def create_app(test_config=None):
       current_questions = paginate_questions(request, selection)
       categories_selection =  Category.query.order_by(Category.id).all()
       categories = {category.id:category.type for category in categories_selection}
-      current_categories = [ question["category"] for question in current_questions ]
+      current_categories = [ categories[{}.format(question["category"])] for question in current_questions ]
       print (current_categories)
 
       if len(current_questions) == 0:
@@ -68,7 +68,7 @@ def create_app(test_config=None):
           'questions' : current_questions,
           'totalQuestions': len(selection),
           'categories': categories,
-          'currentCategory':'History'
+          'currentCategory':current_categories
 
         
         }
