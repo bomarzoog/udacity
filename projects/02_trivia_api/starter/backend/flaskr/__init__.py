a93503779c8f85e383b08f9d2936f7fd3c9f8a73
Bomarzoog <bomarzoogo@gmail.com>
2022-02-03 13:18:38 -0600

    J

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -195,17 +195,17 @@ def create_app(test_config=None):
 
     @app.errorhandler(404)
     def not_found(error):
-      return {
+      return (
           jsonify({"success": False, "error":404, "message": "resource not found"}),
           404
-      }
+      )
 
     @app.errorhandler(422)
     def unprocessable(error):
-        return {
+        return (
             jsonify({"success": False, "error": 422, "message": "unprocessable"}),
             422
-        }
+        )
      
             
 
